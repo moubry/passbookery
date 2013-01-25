ba43dbb87cf02b1062f9c557618e68d44d4b0c6f
Sean Moubry <sean@moubry.com>
2013-01-25 10:18:14 -0600

    Add a new Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passbookery::Application.config.secret_token = '8dc6f2fb949b268d1f79fb3068f9a996fdcab03ddec663affdf800d8db4403c93a514692174dd529520ed7ad64f21de8d97811bea141ded0bb143f9dcc889326'
