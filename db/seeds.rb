f56aa1ceb3a622e504436ee4401fe054131f0d2f
Sean Moubry <sean@moubry.com>
2013-01-25 13:38:54 -0600

    Better seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-pass = Passbook::Pass.create(pass_type_identifier: "com.company.pass.example", serial_number: "ABC123", authentication_token: "XYZ456")
+pass = Passbook::Pass.create(
+  pass_type_identifier: "pass.pbj.www",
+  serial_number: "123456",
+  authentication_token: "1234567890123456"
+)
+
 pass.data = {
   foo: 57,
   bar: Time.now,
   baz: "Lorem ipsum dolar sit amet"
 }
+
 pass.save
 
-pass.registrations.create(device_library_identifier: "123456789", push_token: "00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000")
+pass.registrations.create(
+  device_library_identifier: "123456789",
+  push_token: "00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000"
+)
